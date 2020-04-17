c3fd2c269fa2722235643fc02ef3fbfc14dd0f64
Enrico Grillo <enrico@redbassett.com>
2020-04-17 01:09:35 -0400

    Display loaded list

--- app/src/main/java/com/redbassett/angrybirdsmvi/ui/list/ListActivity.kt
+++ app/src/main/java/com/redbassett/angrybirdsmvi/ui/list/ListActivity.kt
@@ -1,10 +1,19 @@
 package com.redbassett.angrybirdsmvi.ui.list
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.redbassett.angrybirdsmvi.AngryBirdsApp
 import com.redbassett.angrybirdsmvi.R
+import com.redbassett.angrybirdsmvi.data.model.Bird
 import com.redbassett.angrybirdsmvi.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -12,6 +21,9 @@ import javax.inject.Inject
 class ListActivity : BaseActivity<ListState>() {
 
     @Inject lateinit var presenter: ListPresenter
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         (applicationContext as AngryBirdsApp).appComponent.inject(this)
@@ -21,6 +33,14 @@ class ListActivity : BaseActivity<ListState>() {
         setContentView(R.layout.activity_main)
 
         presenter.bindView(this)
+
+        viewManager = LinearLayoutManager(this)
+        viewAdapter = BirdListViewAdapter()
+        bird_list.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
 
     override fun render(state: ListState) {
@@ -28,10 +48,12 @@ class ListActivity : BaseActivity<ListState>() {
             is Loading -> {
                 loading_bar.visibility = View.VISIBLE
                 error_group.visibility = View.GONE
+                bird_list.visibility = View.GONE
             }
             is Error -> {
                 loading_bar.visibility = View.GONE
                 error_group.visibility = View.VISIBLE
+                bird_list.visibility = View.GONE
                 error_message.text = state.error.localizedMessage
                 Toast
                     .makeText(this, state.error.localizedMessage, Toast.LENGTH_LONG)
@@ -40,11 +62,47 @@ class ListActivity : BaseActivity<ListState>() {
             is Content -> {
                 loading_bar.visibility = View.GONE
                 error_group.visibility = View.GONE
-                Toast
-                    .makeText(this, "Content loaded", Toast.LENGTH_LONG)
-                    .show()
-                // TODO: 3/31/20 Load content into recyclerview
+                bird_list.visibility = View.VISIBLE
+                (viewAdapter as BirdListViewAdapter).apply {
+                    birds = state.birds
+                    notifyDataSetChanged()
+                }
             }
         }
     }
+}
+
+class BirdListViewAdapter
+    : RecyclerView.Adapter<BirdListViewAdapter.BirdViewHolder>() {
+
+    var birds: List<Bird> = arrayListOf()
+
+    class BirdViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.findViewById(R.id.bird_name)
+        val description: TextView = view.findViewById(R.id.bird_description)
+        val image: ImageView = view.findViewById(R.id.bird_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BirdViewHolder {
+        val rootView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.bird_list_item, parent, false) as ConstraintLayout
+
+        return BirdViewHolder(rootView)
+    }
+
+    override fun onBindViewHolder(holder: BirdViewHolder, position: Int) {
+        val bird = birds[position]
+        holder.apply {
+            name.text = bird.name
+            description.text = bird.description
+
+            Glide
+                .with(this.itemView)
+                .load("https://redbassett-angrybirds.builtwithdark.com/img/${bird.image}.jpg")
+                .centerCrop()
+                .into(image)
+        }
+    }
+
+    override fun getItemCount(): Int = birds.count()
 }
